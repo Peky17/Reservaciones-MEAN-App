3eb05a61a42083f1d14c6d1b16f080e745b765c0
Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2023-08-17 13:55:48 -0600

    Configurar rutas de login y maquetar login

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterScreenComponent } from './pages/register-screen/register-screen.component';
 
-const routes: Routes = [{
-
-}];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterScreenComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
