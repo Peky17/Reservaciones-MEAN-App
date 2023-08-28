74f5687776088b183ef59bd00398f39e274f8f9d
Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2023-08-27 18:55:22 -0600

    Obtener usuario del localStorage

--- src/app/task/pages/main-task/main-task.component.ts
+++ src/app/task/pages/main-task/main-task.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-main-task',
   templateUrl: './main-task.component.html',
-  styleUrls: ['./main-task.component.css']
+  styleUrls: ['./main-task.component.css'],
 })
 export class MainTaskComponent {
+
+  user: any;
+
+  constructor(private authService: AuthService ) {
+    this.user = JSON.parse(localStorage.getItem('user') || "");
+    console.log(this.user);
+
+  }
 
 }
