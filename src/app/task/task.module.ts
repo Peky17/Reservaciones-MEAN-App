e176a20a5afa1c631e1432987e7b0306625b5699
Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2023-09-01 19:02:09 -0600

    creacion de componentes compartidos (dashboard)

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { TaskRoutingModule } from './task-routing.module';
 import { MainTaskComponent } from './pages/main-task/main-task.component';
+import { NavbarComponent } from './pages/main-task/shared/navbar/navbar.component';
+import { FooterComponent } from './pages/main-task/shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    MainTaskComponent
+    MainTaskComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     TaskRoutingModule
   ],
   exports: [
-    MainTaskComponent
+    MainTaskComponent,
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class TaskModule { }
