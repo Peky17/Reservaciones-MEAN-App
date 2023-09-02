48d131d5e723980189d53cf9141bcc59c2a73d32
Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2023-09-01 20:07:18 -0600

    Modals components

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -5,13 +5,17 @@ import { TaskRoutingModule } from './task-routing.module';
 import { MainTaskComponent } from './pages/main-task/main-task.component';
 import { NavbarComponent } from './pages/main-task/shared/navbar/navbar.component';
 import { FooterComponent } from './pages/main-task/shared/footer/footer.component';
+import { DeleteModalComponent } from './pages/modals/delete-modal/delete-modal.component';
+import { UpdateModalComponent } from './pages/modals/update-modal/update-modal.component';
 
 
 @NgModule({
   declarations: [
     MainTaskComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DeleteModalComponent,
+    UpdateModalComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +24,9 @@ import { FooterComponent } from './pages/main-task/shared/footer/footer.componen
   exports: [
     MainTaskComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DeleteModalComponent,
+    UpdateModalComponent
   ]
 })
 export class TaskModule { }
