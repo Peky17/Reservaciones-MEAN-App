8f4aef21c6677e3539eabad6f2d79e9ba3461135
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-04-09 09:03:56 -0600

    Create services for museos, cines & teatros

---
+++ src/app/services/teatro.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeatroService {
+  private baseUrl = environment.baseUrl;
+
+  private _user: any = JSON.parse(localStorage.getItem('user')!);
+
+  get user() {
+    return this._user;
+  }
+  constructor(private httpClient: HttpClient) {}
+}
