d0454a41429ca38ed83ecc8c2a54f075346b3a05
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-04-19 23:12:00 -0600

    Crear componente para renderizar peliculas

--- src/app/cines/cines.module.ts
+++ src/app/cines/cines.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../shared/shared.module';
 import { ReservarCineComponent } from './pages/reservar-cine/reservar-cine.component';
 import { CinesComponent } from './cines.component';
 import { ViewCinesComponent } from './pages/view-cines/view-cines.component';
+import { CardSliderComponent } from './pages/components/card-slider/card-slider.component';
 
 @NgModule({
-  declarations: [CinesComponent, ReservarCineComponent, ViewCinesComponent],
+  declarations: [CinesComponent, ReservarCineComponent, ViewCinesComponent, CardSliderComponent],
   imports: [CommonModule, CinesRoutingModule, SharedModule],
 })
 export class CinesModule {}
--- src/app/cines/pages/reservar-cine/reservar-cine.component.ts
+++ src/app/cines/pages/reservar-cine/reservar-cine.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Stepper from 'bs-stepper';
 
 @Component({
   selector: 'app-reservar-cine',
   templateUrl: './reservar-cine.component.html',
-  styleUrls: ['./reservar-cine.component.css']
+  styleUrls: [
+    './reservar-cine.component.css',
+    '../../../../assets/css/bs-stepper.css',
+  ],
 })
 export class ReservarCineComponent {
+  // Mensaje para cerrar pestaña
+  // @HostListener('window:beforeunload', ['$event'])
+  // handleBeforeUnload(event: any): void {
+  //   const mensaje =
+  //     '¿Estás seguro de abandonar la página? Los cambios no se guardarán.';
+  //   event.returnValue = mensaje;
+  // }
 
+  constructor(private route: ActivatedRoute) {}
+
+  private stepper: Stepper | undefined;
+  id_cine: string | null = this.route.snapshot.paramMap.get('id');
+
+  ngOnInit() {
+    // Stepper
+    const stepperElement = document.querySelector('#stepper1');
+    if (stepperElement) {
+      this.stepper = new Stepper(stepperElement, {
+        linear: true,
+        animation: true,
+      });
+    } else {
+      console.error('Elemento con ID "stepper1" no encontrado.');
+    }
+  }
+
+  next() {
+    if (this.stepper) {
+      this.stepper.next();
+    }
+  }
+
+  previous() {
+    if (this.stepper) {
+      this.stepper.previous();
+    }
+  }
 }
