9c0005f935117742aee7f057f3bad8e0931b5f2e
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-04-22 23:50:33 -0600

    Desarrollar invoice component

--- src/app/cines/pages/components/invoice/invoice.component.ts
+++ src/app/cines/pages/components/invoice/invoice.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReservarCineComponent } from '../../reservar-cine/reservar-cine.component';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-cine-invoice',
@@ -8,11 +9,46 @@ import { ReservarCineComponent } from '../../reservar-cine/reservar-cine.compone
 })
 export class InvoiceComponent {
   fechaActual: Date;
-  constructor(private reservarCineComponent: ReservarCineComponent) {
+  id_usuario: string = '';
+  username: string = '';
+  emision: string = new Date().toLocaleString('es-ES', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  datosCompartidos: any;
+
+  precio: number = 0;
+  qtySeats: number = 0;
+  name: string = '';
+  formatedSeats: any[] = [];
+
+  constructor(
+    private reservarCineComponent: ReservarCineComponent,
+    private dataSharingService: DataSharingService
+  ) {
     this.fechaActual = new Date();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const user = localStorage.getItem('user');
+    this.id_usuario = JSON.parse(user!).id;
+    this.username = JSON.parse(user!).username;
+
+    this.dataSharingService.dataObservable.subscribe((data) => {
+      this.datosCompartidos = JSON.parse(data);
+      this.precio = this.datosCompartidos.price;
+      this.qtySeats = this.datosCompartidos.seats.length;
+      this.name = this.datosCompartidos.selection;
+
+      // Cambia el primer elemento de cada par [X, Y] por la letra correspondiente
+      this.formatedSeats = this.cambiarPrimerElemento(
+        this.datosCompartidos.seats
+      );
+    });
+  }
 
   previous() {
     this.reservarCineComponent.previous();
@@ -72,5 +108,32 @@ export class InvoiceComponent {
     return (
       fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getFullYear()
     );
+  }
+
+  cambiarPrimerElemento(seats: any[]): any[] {
+    const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Alfabeto
+
+    try {
+      return seats.map((seat) => {
+        if (
+          typeof seat === 'object' &&
+          seat !== null &&
+          'row' in seat &&
+          'seat' in seat
+        ) {
+          const x = seat.row;
+          const y = seat.seat;
+
+          const letra = letras.charAt(x + y);
+
+          return [letra, y];
+        } else {
+          throw new Error('El objeto seat no tiene la estructura esperada');
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 }
